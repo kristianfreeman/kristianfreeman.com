f37f89fc90cca8212912afd1ffb41f549db809cf
Kristian Freeman <kristian@kristianfreeman.com>
2024-10-23 16:39:23 -0500

    2024-10-23-16:39:23

--- worker/index.ts
+++
@@ -1,9 +0,0 @@
-interface Env {
-  ASSETS: Fetcher;
-}
-
-export default {
-  async fetch(request, env): Promise<Response> {
-    return env.ASSETS.fetch(request);
-  }
-} satisfies ExportedHandler<Env>;
