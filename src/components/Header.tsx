ff0cb501a395e52178890e6048075397c7f47388
Kristian Freeman <kristian@kristianfreeman.com>
2024-10-23 09:11:37 -0500

    2024-10-23-09:11:37

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import type { CollectionEntry } from 'astro:content';
-
+import clsx from 'clsx';
 import HeaderLink from './HeaderLink.tsx';
 import { SITE_TITLE } from '../consts';
 
@@ -8,29 +8,31 @@ type BlogPost = CollectionEntry<'blog'>['data'];
 export default ({ post, url }: { post?: BlogPost, url: URL }) => (
   <header className="space-y-4 mb-12">
     <nav className="flex items-center gap-4">
-      <a className="font-bold flex-1 text-primary" href="/">
+      <a className={clsx(url.pathname == "/" ? "underline decoration-dashed" : "", "font-bold flex-1 text-primary")} href="/">
         {SITE_TITLE}
       </a>
 
-      <HeaderLink active={url.pathname.startsWith("/blog") || post && !post.tags.includes('tip')} href="/blog">
-        Blog
-      </HeaderLink>
+      <div className="hidden md:flex items-center gap-4">
+        <HeaderLink active={url.pathname.startsWith("/blog") || post && !post.tags.includes('tip')} href="/blog">
+          Blog
+        </HeaderLink>
 
-      <HeaderLink active={url.pathname.startsWith("/tags/tip") || post && post.tags.includes('tip')} className="hidden md:block" href="/tags/tip">
-        Tips
-      </HeaderLink>
+        <HeaderLink active={url.pathname.startsWith("/tags/tip") || post && post.tags.includes('tip')} className="hidden md:block" href="/tags/tip">
+          Tips
+        </HeaderLink>
 
-      <a href="https://twitter.com/kristianf_" target="_blank" className="nav-link hidden md:block">
-        @kristianf_
-      </a>
+        <a href="https://twitter.com/kristianf_" target="_blank" className="nav-link">
+          @kristianf_
+        </a>
 
-      <a href="https://kristianf.dev/youtube" target="_blank" className="nav-link hidden md:block">
-        YouTube
-      </a>
+        <a href="https://kristianf.dev/youtube" target="_blank" className="nav-link">
+          YouTube
+        </a>
 
-      <a href="https://github.com/kristianfreeman" target="_blank" className="nav-link hidden md:block">
-        GitHub
-      </a>
+        <a href="https://github.com/kristianfreeman" target="_blank" className="nav-link">
+          GitHub
+        </a>
+      </div>
     </nav>
   </header>
 )
