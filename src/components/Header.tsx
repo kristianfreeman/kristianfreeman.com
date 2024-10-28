3644e1e7cdb48d220b5c3a0577e821d022e76814
Kristian Freeman <kristian@kristianfreeman.com>
2024-10-28 11:04:04 -0500

    bonkers anims

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ type BlogPost = CollectionEntry<'blog'>['data'];
 
 export default ({ post, url }: { post?: BlogPost, url: URL }) => {
   return (
-    <header className="h-16 pt-8 space-y-4 mb-12">
+    <header className="space-y-4 p-8 w-full">
       <nav className="flex items-center gap-4">
         <div className="flex-1">
           <a className={clsx("text-foreground font-bold flex-1 hover:no-underline")} href="/">
