0f0c5b481a8b6847814a3adff84589c59618d50a
Kristian Freeman <kristian@kristianfreeman.com>
2024-11-01 16:31:15 -0500

    2024-11-01-16:31:15

---
+++ src/components/StreamVideo.tsx
@@ -0,0 +1,27 @@
+import { Suspense, useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Stream } from "@cloudflare/stream-react";
+
+export default function StreamVideo({ src }: { src: string }) {
+  const [animationDelayComplete, setAnimationDelayComplete] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimationDelayComplete(true);
+    }, 500);
+  }, []);
+
+  if (!animationDelayComplete) {
+    return <Skeleton className="h-[400px] rounded-none w-full" />
+  };
+
+  return (
+    <div className="h-[400px] w-full">
+      <Stream
+        controls
+        defaultTextTrack="en"
+        src={src}
+      />
+    </div>
+  );
+}
