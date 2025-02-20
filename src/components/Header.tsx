09f5c757f50f1ae65a2d372c95c4d41414db90cb
Kristian Freeman <kristian@kristianfreeman.com>
2025-02-20 09:38:00 -0600

    2025-02-20-09:38:00

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import type { CollectionEntry } from 'astro:content';
 import clsx from 'clsx';
 import HeaderLink from './HeaderLink.tsx';
 import { SITE_BRAND } from '../consts';
-import { Newspaper, Paperclip, Search } from 'lucide-react';
+import { Search } from 'lucide-react';
 
 type BlogPost = CollectionEntry<'blog'>['data'];
 
@@ -12,7 +12,7 @@ export default ({ post, url }: { post?: BlogPost, url: URL }) => {
       <nav className="flex items-center gap-4">
         <div className="flex-1">
           <a 
-            className={clsx("text-white flex-1 font-bold tracking-widest hover:no-underline text-lg bg-slate-900 rounded-md p-2 hover:bg-slate-700 transition-colors")}
+            className={clsx("text-white flex-1 font-bold tracking-widest hover:no-underline text-lg bg-slate-900 rounded-md p-2 hover:bg-slate-800 transition-colors")}
             href="/"
           >
             {SITE_BRAND}
@@ -21,17 +21,15 @@ export default ({ post, url }: { post?: BlogPost, url: URL }) => {
 
         <div className="flex items-center gap-8 uppercase tracking-wide">
           <HeaderLink active={url.pathname.startsWith("/blog") || !!post} href="/blog">
-            <Newspaper size={14} />
             Blog
           </HeaderLink>
 
           <HeaderLink active={url.pathname.startsWith("/links")} className="hidden md:flex" href="/links">
-            <Paperclip size={14} />
             Links
           </HeaderLink>
 
           <span className="hidden md:flex items-center gap-1 text-foreground/60">
-            <Search size={16} /> ⌘+K
+            <Search size={16} />: ⌘+K
           </span>
         </div>
       </nav>
